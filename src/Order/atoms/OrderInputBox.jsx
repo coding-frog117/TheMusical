86366dc2e812144d80b3e9e862b50ba767a48ca9
coding-frog117 <syeshe2@gmail.com>
2023-07-29 23:54:54 +0900

    Design : 주문/결제 페이지 주문자 정보 구현

--- src/Order/atoms/OrderInputBox.jsx
+++ src/Order/atoms/OrderInputBox.jsx
@@ -4,7 +4,6 @@ const OrderInputBox = styled.div`
     width : 100%;
     height : 56px;
     display : flex;
-    gap : 140px;
     align-items : center;
     padding : 8px 0;
     border-bottom : 1px solid ${(props)=>props.theme.lightGray};
--- src/Order/molecules/OrderEmailInput.jsx
+++ src/Order/molecules/OrderEmailInput.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import OrderInputBox from '../atoms/OrderInputBox'
 import MiddleBlank from '../atoms/MiddleBlank'
 import OrderInputTitle from '../atoms/OrderInputTitle'
+import OrderInputBox from '../atoms/OrderInputBox'
 
-export default function OrderNameInput() {
+export default function OrderEmailInput() {
   return (
     <OrderInputBox>
-        <OrderInputTitle text = "이름"/>
+        <OrderInputTitle text = "이메일"/>
         <MiddleBlank />
     </OrderInputBox>
   )
