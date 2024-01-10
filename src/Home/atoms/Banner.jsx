aca9c1d08768341d63a517f2870f4c46821cd5ec
coding-frog117 <syeshe2@gmail.com>
2024-01-10 21:54:44 +0900

    Feat : 베너 이미지 불러오기 기능 구현

--- src/Home/atoms/Banner.jsx
+++ src/Home/atoms/Banner.jsx
@@ -1,21 +1,52 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
-// import convert from 'xml-js';
+import BannerLayout from './BannerLayout';
+import { xmlToJson } from '../../xmlToJson/xmlToJson';
 
 const Img = styled.img`
-	width: 100%;
-	height: 500px;
+	height: 320px;
+`;
+
+const Caption = styled.figcaption`
+	font-size: ${(props) => props.theme.md};
+	font-weight: 500;
+	text-align: center;
+	margin-top: 10px;
 `;
 
 export default function Banner() {
 	const ServiceKey = '2a7c8f0ba63c4661b6a06dfdca5182a6';
-	const getUrl = `/openApi/restful/boxoffice?service=${ServiceKey}&ststype=day&date=20240102&catecode=AAAA&area=11`;
+	const getUrl = `http://kopis.or.kr/openApi/restful/boxoffice?service=${ServiceKey}&ststype=week&date=20240110&catecode=GGGA`;
+	const getDetailUrl = `http://www.kopis.or.kr/openApi/restful/pblprfr/PF227003?service=${ServiceKey}&newsql=Y`;
+	const [boxoffice, setBoxOffice] = useState([]);
+
+	const getdata = async () => {
+		const response = await fetch(getUrl);
+
+		const xmlString = await response.text();
+		const XmlNode = new DOMParser().parseFromString(xmlString, 'text/xml');
+
+		// import해온 xmlToJson함수 안에 변형한 XmlNode를 넣어준다. 그러면 json객체를 return해준다.
+		const item = xmlToJson(XmlNode);
+		setBoxOffice(item.boxofs.boxof);
+	};
 
 	useEffect(() => {
-		fetch(getUrl, { method: 'GET' }).then((res) => {
-			console.log(res);
-		});
+		getdata();
 	}, []);
 
-	return <Img />;
+	return (
+		<BannerLayout>
+			{boxoffice.map((_, index) => {
+				if (index < 10) {
+					return (
+						<figure>
+							<Img src={`http://www.kopis.or.kr` + boxoffice[index].poster} />
+							<Caption>{boxoffice[index].prfnm}</Caption>
+						</figure>
+					);
+				}
+			})}
+		</BannerLayout>
+	);
 }
