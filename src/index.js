dafcb090e0f9f9424454fdf264cdbfc97b922496
coding-frog117 <syeshe2@gmail.com>
2024-01-21 15:43:50 +0900

    Chore : meta 태그 추가

--- src/index.js
+++ src/index.js
@@ -1,27 +1,27 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
-import { ThemeProvider } from "styled-components";
-import theme from "./styles/theme";
-import GlobalStyles from "./styles/GlobalStyles";
-import { Provider } from "react-redux";
-import store from './store.js'
-import persistStore from "redux-persist/es/persistStore";
-import { PersistGate } from "redux-persist/integration/react";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import { ThemeProvider } from 'styled-components';
+import theme from './styles/theme';
+import GlobalStyles from './styles/GlobalStyles';
+import { Provider } from 'react-redux';
+import store from './store.js';
+import persistStore from 'redux-persist/es/persistStore';
+import { PersistGate } from 'redux-persist/integration/react';
 
 const persistor = persistStore(store);
 
 ReactDOM.render(
-  <Provider store = {store}>
-    <PersistGate loading = {null} persistor={persistor}>
-      <ThemeProvider theme={theme}>
-        <GlobalStyles />
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </ThemeProvider>
-    </PersistGate>
-  </Provider>,
-  document.getElementById("root"),
+	<Provider store={store}>
+		<PersistGate loading={null} persistor={persistor}>
+			<ThemeProvider theme={theme}>
+				<GlobalStyles />
+				<BrowserRouter basename={process.env.PUBLIC_URL}>
+					<App />
+				</BrowserRouter>
+			</ThemeProvider>
+		</PersistGate>
+	</Provider>,
+	document.getElementById('root')
 );
